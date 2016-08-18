d67468212ab10fbb615548b79f4cb41cc744aa71
Aurélien GALTIER <aurelien.galtier@live.fr>
2016-08-18 21:15:20 +0200

    Add resolver from Pattern

--- src/SpecIt.Pattern/ScenarioPattern.cs
+++ src/SpecIt.Pattern/ScenarioPattern.cs
@@ -1,14 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace SpecIt.Pattern
+﻿namespace SpecIt.Pattern
 {
     public class ScenarioPattern : Scenario
     {
         public ScenarioPattern()
-            : base(new Kernel())
+            : base(new PatternResolver())
         {
         }
     }

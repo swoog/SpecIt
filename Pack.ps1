0d4aadb4652b35ae5370d0dc694ea9c5c22acb51
Aurélien GALTIER <aurelien.galtier@live.fr>
2016-08-19 10:30:14 +0200

    Fix pack.ps1

--- Pack.ps1
+++ Pack.ps1
@@ -5,9 +5,9 @@ Write-Output "Pack all projects"
 $projects = Get-ChildItem -Path "src" -Recurse "Project.json"
 Write-Output $projects
 foreach($project in $projects){
-	if (!$project.FullName.EndsWith("Tests"))
+	if (!$project.FullName.EndsWith("Tests\Project.json"))
 	{
-		Write-Output "otnet pack -c Release --no-build $($project.FullName) --version-suffix $versionNumber"
+		Write-Output "dotnet pack -c Release --no-build $($project.FullName) --version-suffix $versionNumber"
 		dotnet pack -c Release --no-build $($project.FullName) --version-suffix $versionNumber
 	}
 }

c1c34fbe587c22995e3b205b6ec555ad1fb28e93
Aurélien GALTIER <aurelien.galtier@live.fr>
2016-08-19 16:33:36 +0200

    Fix pack.json

--- Pack.ps1
+++ Pack.ps1
@@ -5,7 +5,7 @@ Write-Output "Pack all projects"
 $projects = Get-ChildItem -Path "src" -Recurse "Project.json"
 Write-Output $projects
 foreach($project in $projects){
-	if (!$project.FullName.EndsWith("Tests\\Project.json"))
+	if (!$project.FullName.EndsWith("Tests\\project.json"))
 	{
 		Write-Output "dotnet pack -c Release --no-build $($project.FullName) --version-suffix $versionNumber"
 		dotnet pack -c Release --no-build $($project.FullName) --version-suffix $versionNumber
